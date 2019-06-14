b72834dfb0f509f5c010a27750a3b809f63942d4
Mrchiantos <51173452+Mrchiantos@users.noreply.github.com>
2019-06-14 22:11:27 +0200

    1.72e

--- mods/Menu/hud/init.lua
+++ mods/Menu/hud/init.lua
@@ -1,16 +1,14 @@
-
 hud = {}
 
 local path = minetest.get_modpath("hud")
-
-dofile(path .. "/api.lua")
-dofile(path .. "/builtin.lua")
-dofile(path .. "/legacy.lua")
 
 if PLATFORM == "Android" then
 
 else
 
-	dofile(path .. "/itemwheel.lua")
+dofile(path .. "/api.lua")
+dofile(path .. "/builtin.lua")
+dofile(path .. "/legacy.lua")
+dofile(path .. "/itemwheel.lua")
 	
 end
